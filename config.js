1d12d3e15b6a15b946a58267810508a025a225a1
not1cyyy <firas1120@gmail.com>
2023-12-31 00:41:49 +0100

    Initiall commit

---
+++ config.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+    "registercommands": true, //Write True If You Are Launching The Bot First Time
+    "token": "MTE5MDI5NTc0ODMxNTcxNzY3Mw.GcLA-f.2AX0Usf-RizVa5glDvmW26tSnM3nTmA2kMAdS4", //Your Super Secret Bot Token
+    "imageapi": "", //Your Amethyste Api You can get it from https://api.amethyste.moe/
+    "ownerID": [553317144746655787], //Your Discord User ID
+    "prefix": ".", //Your Bot's Prefix
+    "chat": {
+        "url": "",
+        "bid": "",
+        "key": "",
+        "uid": ""
+    }, // You can get This Things from https://brainshop.ai
+    "api": "AIzaSyD3lCq72iP1-Gy-ZWB8L9bb3XfEYYJTML8", // Your Youtube Api
+    "youtubeAPI": "AIzaSyD3lCq72iP1-Gy-ZWB8L9bb3XfEYYJTML8", // Your Youtube Api
+    mainprefix: ".",  // Again Your Prefix
+    "owner": "not1cyyy", // Owner Name
+
+    basiclang: "en", //The basic language of the bot, "fr" for French and "en" for English
+    embeds: {
+        color: "BLUE", //Embed color (in English)
+        footers: "GIVEAWAY :tada: :tada:" //Embed footer
+    },
+
+
+
+    events: {
+        addcolor: "GREEN", //The color of the event add (in English)
+        remcolor: "RED" //The color of the event remove (in English)
+    },
+
+    reaction: "🎉", //Reaction to the giveaways if you in the console you see 'unknown emoji' that's what this emoji is not recognized by Discord
+
+    grole: "Giveaway Manager", //If the member doesn't have permission to handle messages he can still use the giveaways commands if he has the role configured right here
+
+    auth: {
+        support: "https://discord.gg/ERuYpfWAQJ", //The link of your Discord server
+        dperms: "8" //The permissions that the bot asks on we want to add it on a Discord server (8 = moderator)
+    },
+} 
