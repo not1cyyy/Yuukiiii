06237aad3a4c4c5b9e6e55cdc9907b3cd7ca7388
not1cyyy <firas1120@gmail.com>
2024-01-05 01:16:53 +0100

    some improvements

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -2,45 +2,69 @@ const db = require('quick.db')
 const Discord = require('discord.js')
 const ms = require("parse-ms");
 
-    module.exports = {
+module.exports = {
   name: "work",
   aliases: [],
-  execute: async(client, message, args) => {
+  execute: async (client, message, args) => {
 
     let user = message.author;
     let author = await db.fetch(`work_${message.author.id}`)
 
     let timeout = 600000;
-    
+
     if (author !== null && timeout - (Date.now() - author) > 0) {
-        let time = ms(timeout - (Date.now() - author));
-    
-        let timeEmbed = new Discord.MessageEmbed()
+      let time = ms(timeout - (Date.now() - author));
+
+      let timeEmbed = new Discord.MessageEmbed()
         .setColor("RANDOM")
         .setDescription(`:x:  You have already worked recently\n\nTry again in ${time.minutes}m ${time.seconds}s `);
-        message.channel.send(timeEmbed)
-      } else {
+      message.channel.send(timeEmbed)
+    } else {
 
-        let replies = ['Programmer','Builder','Waiter','Busboy','Chief','Mechanic']
+      let jobs = [
+        'Programmer', 'Builder', 'Waiter', 'Busboy', 'Chef', 'Mechanic', 'Engineer',
+        'Teacher', 'Doctor', 'Nurse', 'Artist', 'Police Officer', 'Firefighter',
+        'Dentist', 'Pharmacist', 'Plumber', 'Electrician', 'Architect', 'Pilot',
+        'Veterinarian', 'Graphic Designer', 'Financial Analyst', 'Real Estate Agent',
+        'Librarian', 'Writer', 'Journalist', 'Chef', 'Barista', 'Accountant',
+        'Physical Therapist', 'Chemist', 'Biologist', 'Psychologist', 'Actor',
+        'Marketing Manager', 'Sales Representative', 'Human Resources Manager',
+        'Event Planner', 'Flight Attendant', 'Musician', 'Economist', 'Professor',
+        'Social Worker', 'Software Developer', 'Interior Designer', 'Mechanical Engineer',
+        'Civil Engineer', 'Aerospace Engineer', 'Data Scientist', 'Chef', 'Geologist',
+        'Fashion Designer', 'Photographer', 'Dietitian', 'Athletic Trainer', 'Translator',
+        'IT Manager', 'Electrician', 'Paralegal', 'Executive Assistant', 'Orthodontist',
+        'Forensic Scientist', 'Meteorologist', 'Marine Biologist', 'Embryologist',
+        'Zoologist', 'Orthopedic Surgeon', 'Radiologist', 'Pediatrician', 'Surgeon',
+        'Anesthesiologist', 'Neurologist', 'Cardiologist', 'Oncologist', 'Gastroenterologist',
+        'Endocrinologist', 'Hematologist', 'Optometrist', 'Chiropractor', 'Podiatrist',
+        'Audiologist', 'Speech Therapist', 'Veterinary Technician', 'Dental Hygienist',
+        'Phlebotomist', 'Radiation Therapist', 'Surgical Technologist', 'EMT', 'Paramedic',
+        'Flight Paramedic', 'Pharmacy Technician', 'Medical Assistant', 'Dietary Aide',
+        'Physical Therapy Assistant', 'Occupational Therapy Assistant', 'Cardiovascular Technologist',
+        'Diagnostic Medical Sonographer', 'Respiratory Therapist', 'Orthotist', 'Prosthetist'
+      ];
 
-        let result = Math.floor((Math.random() * replies.length));
-        let amount = Math.floor(Math.random() * 80) + 1;
-        let embed1 = new Discord.MessageEmbed()
+
+      let result = Math.floor((Math.random() * jobs.length));
+      let amount = Math.floor(Math.random() * 100) + 1;
+      let embed1 = new Discord.MessageEmbed()
         .setColor("RANDOM")
-        .setDescription(`:white_check_mark: You worked as a ${replies[result]} and earned ${amount} coins`);
-        message.channel.send(embed1)
-        
-        db.add(`money_${message.author.id}`, amount)
-        db.set(`work_${message.author.id}`, Date.now())
+        .setImage("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+        .setDescription(`:white_check_mark: You worked as a ${jobs[result]} and earned ${amount} coins`);
+      message.channel.send(embed1)
+
+      db.add(`money_${message.author.id}`, amount)
+      db.set(`work_${message.author.id}`, Date.now())
     };
+  }
+
+
 }
-
-
-    }
 module.exports.help = {
-    name: "work",
-    description: "Work as a Programmer, Builder, Waiter, Busboy, Chied, Mechanic and get money",
-    usage: "work",
-    type: "Economy"  
+  name: "work",
+  description: "Work and get money",
+  usage: "work",
+  type: "Economy"
 }
 
--- commands/fun/kill.js
+++ commands/fun/kill.js
@@ -6,47 +6,64 @@ var deaths = [
   "[NAME1] sent John Wick to kill [NAME2]!",
   "[NAME1] pressed Ctrl+Alt+Del deleting [NAME2] from the Universe!",
   "[NAME1] threw the ban hammer at [NAME2] for spamming",
-  "[NAME2] stepped on a lego brick"
+  "[NAME2] stepped on a lego brick",
+  "[NAME2] couldn't resist how cute [NAME1] is and died!",
+  "[NAME2] was killed by a flying potato",
+  "[NAME2] drowned in a bathtub while trying to catch a rubber ducky",
+  "[NAME2] suffocated while trying to prove that humans can live without oxygen",
+  "[NAME2] saw a spider and died of a heart attack",
+  "[NAME2] heard that the world is going to end, so they ended themselves",
+  "[NAME2] shined a laser pointer at the sun and it reflected back at them",
+  "[NAME2] shot themselves to see if the gun was real",
+  "[NAME2] tried to prove that hell is real",
+  "[NAME2] tried to make a potion that makes them immortal, but it backfired and they died",
+  "[NAME2] overdosed on a pill that was supposed to make them smarter",
 ];
 
 
 module.exports = {
   name: "kill",
   aliases: [],
-  execute: async(client, message, args, data, db) => {
+  execute: async (client, message, args, data, db) => {
     let member = await message.mentions.members.first();
 
     if (!member) {
-        return message.channel.send({embed: {
-            color: 16734039,
-            description: "Mention a valid member of this server!"
-        }})
+      return message.channel.send({
+        embed: {
+          color: 16734039,
+          description: "Mention a valid member of this server!"
+        }
+      })
     }
 
-		if (message.author === member) {
-           return await message.channel.send({embed: {
-                color: 16734039,
-                description: "You cant kick yourself!"
-            }})
-		}
-    var pickeddeath = deaths[Math.floor(Math.random()*deaths.length)];
+    if (message.author.id === member.id) {
+      return await message.channel.send({
+        embed: {
+          color: 16734039,
+          description: "You cant kill yourself!"
+        }
+      })
+    }
+    var pickeddeath = deaths[Math.floor(Math.random() * deaths.length)];
     var change1 = pickeddeath.replace("[NAME1]", message.author.username);
     var change2 = change1.replace("[NAME2]", member.displayName);
-  
-    await message.channel.send({embed: {
+
+    await message.channel.send({
+      embed: {
         color: 16734039,
         author: {
           name: "Tombstone of " + member.displayName + "!",
           icon_url: member.user.displayAvatarURL
         },
-        title: "Cause of Death",    
+        title: "Cause of Death",
         description: change2
-    }});
-}
+      }
+    });
+  }
 }
 module.exports.help = {
-    name: "kill",
-    description: "Murders a user",
-    usage: "kill <user>",
-    type: "Fun" 
+  name: "kill",
+  description: "Murders a user",
+  usage: "kill <user>",
+  type: "Fun"
 }
